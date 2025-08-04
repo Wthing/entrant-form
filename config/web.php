612d47e027270cba684d630d329b6f202f090b03
Wthing <wierd8things@gmail.com>
2025-08-04 16:32:06 +0500

    аbc

--- config/web.php
+++ config/web.php
@@ -70,6 +70,7 @@ $config = [
             'class' => 'mdm\admin\components\AccessControl',
             'allowActions' => [
                 'site/*',
+                'admin/*',
             ]
         ],
 
